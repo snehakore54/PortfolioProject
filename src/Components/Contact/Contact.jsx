81a68f9c3817b2453a676dd3e99d4a1869b57b77
snehakore54 <151037050+snehakore54@users.noreply.github.com>
2024-09-21 13:02:21 +0530

    Update the navigation part

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ import './Contact.css'
 
 const Contact = () => {
   return (
-    <section>
+    <section id="contact">
       <h5>Get in Touch</h5>
       <h2>Contact Me</h2>
 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,48 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
 // eslint-disable-next-line no-unused-vars
-/*import React from 'react'
-import { BsLinkedin } from 'react-icons/bs'
-import { BsGithub } from 'react-icons/bs'
-import { BsMedium } from 'react-icons/bs'
-import './Home.css'
-
-const Home = () => {
-  return (
-    <header id="home">
-        <div className='container home-container'>
-          <h5>Hello, I'm</h5>
-          <h1>Sneha kore</h1>
-          <h5 className='text-light'> Full-stack Software Engineer</h5>
-          <div className='cta'>
-            <a href="" download className='btn'>Download CV</a>
-            <a href="#contact" className='btn btn-primary'>Contact Me</a>
-          </div>
-          <div className="home_social">
-            <a href=''>
-                <BsLinkedin/>
-            </a>
-            <a href=''>
-                <BsGithub/>
-            </a>
-            <a href=''>
-                <BsMedium/>
-            </a>
-          </div>
-
-          <div className="m">
-            <img src='C:\PORTFOLIO\public\images\STK-20240724-WA0009 (1).webp' alt="Kore" />
-          </div>
-         <a href='/contact' className='scroll_down'>
-         Scroll Down
-         </a>
-        </div>
-      
-    </header>
-  )
-}
-
-export default Home*/
-
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import { BsLinkedin, BsGithub, BsMedium } from 'react-icons/bs';
